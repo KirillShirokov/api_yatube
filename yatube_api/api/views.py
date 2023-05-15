ee68aa0ea74de4fac904a6491182244074ead8d2
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-05-15 19:42:43 +0300

    00review

---
+++ yatube_api/api/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from posts.models import Group, Post
+from api.serializers import (GroupSerializer,
+                             PostSerializer,
+                             CommentSerializer)
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    message = 'This is not your content.'
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    permission_classes = [
+        IsAuthorOrReadOnly,
+        permissions.IsAuthenticated,]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsAuthorOrReadOnly,
+        permissions.IsAuthenticated,]
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=post_id)
+        return post.comments
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        author = self.request.user
+        post = get_object_or_404(Post, pk=post_id)
+        serializer.save(author=author, post=post)
